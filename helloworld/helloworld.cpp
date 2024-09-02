93df5786de073fba1081e22833bfa4f9912b9b1e
Junaid388 <junaidkhan.5a4@gmail.com>
2024-09-02 11:45:34 +0530

    explanation of initial hello world

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,16 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <iostream> // pre processor directives starts with #, inclusive
+// #include <vector>
+// #include <string>
 
-using namespace std;
+using namespace std; // using namespace standard, distinguish methods from our defined methods
+
+
+//not loading whole standard only defined
+// using std::cout; 
+// using stf::endl; //line end 'enter'
+
+
+// namespace mySpace{
+//     void display(){
+//         // comment
+//     }
+// }
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // mySpace::display();
+    // for (const string& word : msg)
+    // {
+    //     cout << word << " ";
+    // }
+    // cout << endl;
+    cout << "Hello world first print" << endl;
+    // std::cout <<"Hello";  //not loading whole standard only defined
+    return 0; // return if value 0 as return type is int
 }
